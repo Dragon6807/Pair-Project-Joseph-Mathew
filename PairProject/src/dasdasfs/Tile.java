7b39f1c97ea1332dd8d7e0d572811957c6cba7bf
jwang7517 <jwang7517@CHHB45C4278DE1.inst.hcpss.org>
2017-04-05 09:23:18 -0400

    vxzd

--- PairProject/src/dasdasfs/Tile.java
+++ PairProject/src/dasdasfs/Tile.java
@@ -1,11 +1,22 @@
 package dasdasfs;
 
-import java.swing.;
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.*;;
 
 public class Tile {
-	public int color = 0;
+	public int color;
+	public int coordinateX;
+	public int coordinateY;
 	
-	public  getColor(int rt) {
-		return
+	public Tile(int co, int corx, int cory) {
+		color = co;
+		coordinateX = corx;
+		coordinateY = cory;
+	}
+	
+	public void paint(Graphics g) {
+		g.drawRect(coordinateX, coordinateY, coordinateX+50, coordinateY+50);
 	}
 }
--- PairProject/src/dasdasfs/map.java
+++ PairProject/src/dasdasfs/map.java
@@ -3,4 +3,8 @@ import java.util.*;
 
 public class map {
 	public ArrayList<Tile> arrayMap = new ArrayList();
+	
+	public void create(){
+		arrayMap.add(new Tile(1, 0, 0));
+	}
 }
